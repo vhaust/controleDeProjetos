ce251034f4f37d68b802a3bf22ce1d687546960c
Gabriel <gabr.fern99@gmail.com>
2019-06-12 18:57:14 -0300

    Adiciona classe Alocacao

---
+++ src/main/java/br/com/ap220191/ec04_controle_projetos/model/Alocacao.java
@@ -0,0 +1,13 @@
+package br.com.ap220191.ec04_controle_projetos.model;
+
+public class Alocacao {
+	private int cargaHoraria;
+
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+}
