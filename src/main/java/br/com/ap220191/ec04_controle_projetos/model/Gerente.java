4c8ab439331f02920d22a225f5cb2c0652450de3
expert975 <rodrigocomputadores125@hotmail.com>
2019-06-13 00:10:23 -0300

    Adiciona classe Gerente

---
+++ src/main/java/br/com/ap220191/ec04_controle_projetos/model/Gerente.java
@@ -0,0 +1,10 @@
+package br.com.ap220191.ec04_controle_projetos.model;
+
+public class Gerente extends Colaborador
+{
+	@Override
+	public String toString()
+	{
+		return "Gerente{" + super.toString() + '}';
+	}
+}
