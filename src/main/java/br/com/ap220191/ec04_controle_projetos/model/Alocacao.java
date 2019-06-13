9a5674468569c214971cb3f8e18783db95ff674f
Gabriel <gabr.fern99@gmail.com>
2019-06-13 17:54:08 -0300

    Adiciona classe incompleta Alocacao

--- src/main/java/br/com/ap220191/ec04_controle_projetos/model/Alocacao.java
+++ src/main/java/br/com/ap220191/ec04_controle_projetos/model/Alocacao.java
@@ -1,6 +1,9 @@
 package br.com.ap220191.ec04_controle_projetos.model;
 
 public class Alocacao {
+
+//TODO: Classe incompleta, por favor, complementar a classe
+
 	private int cargaHoraria;
 
 	public int getCargaHoraria() {
